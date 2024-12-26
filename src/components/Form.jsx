0b17ab2d33ad51e317120e66ccc2f2aaedc10a48
dungma <dungma@yunero.io>
2024-12-27 03:23:31 +0700

    wư

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -84,6 +84,7 @@ const FormCP = () => {
           headers: {
             'Content-Type': 'application/json',
           },
+          mode: 'cors',
           body: JSON.stringify(data),
         }
       );
