d995dfd1dcde942bb61df843708e5a526a3754a9
dungma <dungma@yunero.io>
2024-12-28 17:52:20 +0700

    update url

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 
   openGraph: {
     images: [
-      `https://www.dotphaband75.ieltstactics.vn/assets/logo-ielts-tactics.png`,
+      `https://dotphaband75.ieltstactics.vn/assets/logo-ielts-tactics.png`,
     ],
     type: 'website',
     siteName: 'ieltstactics',
